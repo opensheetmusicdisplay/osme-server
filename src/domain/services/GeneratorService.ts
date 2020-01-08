3cd31bffe5e6bb6eb75b66bfed01adb9d6975d52
FloHin <florian@hintermeier.at>
2020-01-08 23:10:19 +0100

    Add GenerateRequest options to generate MusicSheets
    - add request object and validation
    - fix examples for using Array instead of Object notation
    - add parsing for Fraction, PitchSettings, DurationSettings
    - add validation for number constraints

--- src/domain/services/GeneratorService.ts
+++ src/domain/services/GeneratorService.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:variable-name */
 import {Fraction, MusicSheet, RhythmInstruction, RhythmSymbolEnum} from 'opensheetmusicdisplay';
 import {
     DefaultInstrumentOptions,
@@ -11,14 +12,39 @@ import {
     Tone,
     XMLSourceExporter,
 } from 'osme';
+import {GenerateRequest} from '../../routes/Api1';
+import {DistributionEntry} from 'osme/lib/OSME/Common/Distribution';
 
 export class GeneratorService {
 
-    public generateExport(): string {
-        const generatedSheet = this.generateDefault();
+    public generateExport(request: GenerateRequest): string {
+        const generatedSheet = this.generateWithRequest(request);
         const exporter = new XMLSourceExporter();
         const outputTxt = exporter.export(generatedSheet);
         return outputTxt.toString();
+    }
+
+    public generateWithRequest(request: GenerateRequest): MusicSheet {
+        this.validate(request);
+
+        const timeFraction = this.parseFraction(request.time);
+        const scaleTone = this.parseScaleTone(request.scale_tone);
+        const scaleType = this.parseScaleType(request.scale_type);
+        const pitchSettings = this.parsePitchSettings(request.pitch_settings);
+        const durationSettings = this.parseDurationSettings(request.duration_settings);
+
+        const options: SourceGeneratorOptions = {
+            complexity: request.complexity,
+            measure_count: request.measures,
+            tempo: request.tempo,
+            time_signature: new RhythmInstruction(timeFraction, RhythmSymbolEnum.COMMON),
+            scale_key: ScaleKey.create(scaleType, scaleTone),
+            instruments: [DefaultInstrumentOptions.get('Part 1')],
+            pitch_settings: pitchSettings,
+            duration_settings: durationSettings,
+        };
+        const plugin: SourceGeneratorPlugin = new ExampleSourceGenerator(options);
+        return plugin.generate();
     }
 
     public generateDefault(): MusicSheet {
@@ -26,7 +52,10 @@ export class GeneratorService {
             complexity: 0.5,
             measure_count: 5,
             tempo: 145.0,
-            time_signature: new RhythmInstruction(new Fraction(4, 4), RhythmSymbolEnum.COMMON),
+            time_signature: new RhythmInstruction(
+                new Fraction(4, 4),
+                RhythmSymbolEnum.COMMON,
+            ),
             scale_key: ScaleKey.create(ScaleType.MAJOR, Tone.C),
             instruments: [DefaultInstrumentOptions.get('Part 1')],
         };
@@ -34,11 +63,97 @@ export class GeneratorService {
         return plugin.generate();
     }
 
-    public PitchSettings(): PitchSettings {
-        return PitchSettings.HARMONIC();
+    private parsePitchSettings(values: any[]): PitchSettings {
+        const entries = values.map((value) => {
+            const [key, weight] = value;
+            return new DistributionEntry(key, weight);
+        });
+
+        if (entries.length > 0) {
+            return new PitchSettings(0.5, entries);
+        } else {
+            return PitchSettings.HARMONIC();
+        }
     }
 
-    public defaultDurationSettings(): DurationSettings {
-        return DurationSettings.TYPICAL();
+    private parseDurationSettings(values: any[]): DurationSettings {
+        const entries = values.map((value) => {
+            const [key, weight] = value;
+            const fraction = this.parseFraction(key);
+            return new DistributionEntry(fraction, weight);
+        });
+
+        if (entries.length > 0) {
+            return new DurationSettings(0.5, entries);
+        } else {
+            return DurationSettings.TYPICAL();
+        }
     }
+
+    private validate(request: GenerateRequest) {
+        this.assertBetween('complexity', request.complexity, 0.0, 1.0);
+        this.assertBetween('measures', request.measures, 1, 128);
+        this.assertBetween('tempo', request.tempo, 20, 320);
+        this.assetFraction('time', request.time);
+    }
+
+    private assertBetween(name: string, value: number, start: number, end: number) {
+        if (value < start || value > end) {
+            throw new Error(`${name} has a value (${value}) not between ${start} and ${end}`);
+        }
+    }
+
+    private assetFraction(name: string, value: string) {
+        if (!value.includes('/')) {
+            throw new Error(`${name} is not a valid Fraction: ${value} instead of "x/y"`);
+        }
+
+        const splits = value.split('/');
+        this.assertBetween(`$name-Fraction`, Number(splits[0]), 1, 16);
+        this.assertBetween(`$name-Fraction`, Number(splits[1]), 1, 16);
+    }
+
+    private parseFraction(value: string) {
+        const splits = value.split('/');
+        const numerator = Number(splits[0]);
+        const denominator = Number(splits[1]);
+        return new Fraction(numerator, denominator, 0, false);
+    }
+
+    private parseScaleTone(scaleTone: string): Tone {
+        switch (scaleTone.toLowerCase()) {
+            case 'c':
+                return Tone.C;
+            case 'd':
+                return Tone.D;
+            case 'e':
+                return Tone.E;
+            case 'f':
+                return Tone.F;
+            case 'g':
+                return Tone.G;
+            case 'a':
+                return Tone.A;
+            case 'b':
+                return Tone.B;
+            default:
+                return Tone.C;
+        }
+    }
+
+    private parseScaleType(scaleTone: string): ScaleType {
+        switch (scaleTone.toUpperCase()) {
+            case 'MAJOR':
+                return ScaleType.MAJOR;
+            case 'MINOR_HARMONIC':
+                return ScaleType.MINOR_HARMONIC;
+            case 'MINOR_MELODIC':
+                return ScaleType.MINOR_MELODIC;
+            case 'MINOR_NATURAL':
+                return ScaleType.MINOR_NATURAL;
+            default:
+                return ScaleType.MAJOR;
+        }
+    }
+
 }
--- src/routes/Api1.ts
+++ src/routes/Api1.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:variable-name */
 import {logger} from '@shared';
 import {Request, Response, Router} from 'express';
 import {BAD_REQUEST, OK} from 'http-status-codes';
@@ -8,9 +9,21 @@ const router = Router();
 
 const generatorService = new GeneratorService();
 
+export class GenerateRequest {
+    public measures: number = 1;
+    public time: string = '';
+    public complexity: number = 0.1;
+    public tempo: number = 100.0;
+    public scale_tone: string = '';
+    public scale_type: string = '';
+    public pitch_settings: any[] = new Array<any>();
+    public duration_settings: any[] = new Array<any>();
+}
+
 router.post('/sheets/generate', async (req: Request, res: Response) => {
     try {
-        const xmlBody: string = generatorService.generateExport();
+        const request = req.body as GenerateRequest;
+        const xmlBody: string = generatorService.generateExport(request);
         return res.status(OK).send(xmlBody);
     } catch (err) {
         logger.error(err.message, err);
@@ -20,35 +33,31 @@ router.post('/sheets/generate', async (req: Request, res: Response) => {
     }
 });
 
-router.get('/sheets/generate', async (req: Request, res: Response) => {
+router.get('/sheets/generate/example/1', async (req: Request, res: Response) => {
     try {
         return res.status(OK).json({
             measures: 'Number of measures [16]',
-            time_numerator: 'Number of Unit for Time, e.g 4/x [4]',
-            time_denominator: 'Number of Unit for Time FracFviewstion, e.g. x/4 [4]',
+            time: 'Number of Unit for Time, e.g 4/x [4/4]',
             complexity: 'Float between 0.0 and 1.0 to indicate complexity and difficulty [0.5]',
             tempo: 'Tempo in BPM [145]',
             scale_tone: 'Root tone of scale to be used [C]',
             scale_type: 'Scale type of scale [MAJOR]',
             pitch_settings: [
-                {0: '0.1'},
-                {1: '0.1'},
-                {2: '0.1'},
-                {3: '0.1'},
-                {4: '0.1'},
-                {5: '0.1'},
-                {6: '0.1'},
-                {7: '0.1'},
+                [0, 0.3],
+                [1, 0.05],
+                [2, 0.1],
+                [3, 0.1],
+                [4, 0.1],
+                [5, 0.2],
+                [6, 0.05],
+                [7, 0.1],
             ],
             duration_settings: [
-                {'1/16': '0.1'},
-                {1: '0.1'},
-                {2: '0.1'},
-                {3: '0.1'},
-                {4: '0.1'},
-                {5: '0.1'},
-                {6: '0.1'},
-                {7: '0.1'},
+                ['1/16', 0.1],
+                ['1/8', 0.3],
+                ['1/4', 0.3],
+                ['1/2', 0.2],
+                ['1/1', 0.1],
             ],
         });
     } catch (err) {
